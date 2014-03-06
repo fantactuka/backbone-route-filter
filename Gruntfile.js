f618dfadc3dd043dac06cf6db3d204344db81fde
Maksim Horbachevsky <fantactuka@gmail.com>
2014-03-06 13:36:40 +0300

    Setting version to bower.json during release

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,7 @@ module.exports = function(grunt) {
 
     version: {
       update: {
-        src: ['component.json', 'package.json']
+        src: ['bower.json', 'package.json']
       }
     }
   });
